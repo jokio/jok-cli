0c6c66f242c2df5a2a43c6f7206453298d512f7c
Ezeki Zibzibadze <ez@jok.io>
2018-12-21 21:59:52 +0400

    fix: npm usage issue

--- src/commands/init/utils/install.ts
+++ src/commands/init/utils/install.ts
@@ -3,43 +3,40 @@ import * as output from '../../../integration/output'
 import * as messages from '../messages'
 import getInstallCmd from './get-install-cmd'
 
-export default function (opts) {
+export default async function (opts) {
 	const projectName = opts.projectName
 	const projectPath = opts.projectPath
 	const packages = opts.packages || []
 
 	const installCmd = getInstallCmd()
-	const installArgs = packages.length
-		? getInstallArgs(installCmd, packages)
-		: []
 
 	process.chdir(projectPath)
 
-	return new Promise(function (resolve, reject) {
-		const stopInstallSpinner = output.wait('Installing modules')
+	const stopInstallSpinner = output.wait('Installing modules')
 
-		execa(installCmd, installArgs).then(function () {
-			// Confirm that all dependencies were installed
-			return execa(installCmd, ['install'])
-		}).then(function () {
-			stopInstallSpinner()
-			output.success(`Installed dependencies for ${projectName}`)
-			resolve()
-		}).catch(function (err) {
-			console.log(err)
-			stopInstallSpinner()
-			console.log(messages.installError(packages))
-			return reject(new Error(`${installCmd} installation failed`))
-		})
-	})
-}
+	try {
+		const result = await execa(installCmd, ['install'])
 
-function getInstallArgs(cmd, packages) {
-	if (cmd === 'npm') {
-		const args = ['install', '--save', '--save-exact']
-		return args.concat(packages, ['--verbose'])
-	} else if (cmd === 'yarn') {
-		const args = ['add']
-		return args.concat(packages)
+		stopInstallSpinner()
+		output.success(`Installed dependencies for ${projectName}`)
+
+		return result
+	}
+	catch (err) {
+		console.log(err)
+		stopInstallSpinner()
+		console.log(messages.installError(packages))
+
+		throw new Error(`${installCmd} installation failed`)
 	}
 }
+
+// function getInstallArgs(cmd, packages) {
+// 	if (cmd === 'npm') {
+// 		const args = ['install', '--save', '--save-exact']
+// 		return args.concat(packages, ['--verbose'])
+// 	} else if (cmd === 'yarn') {
+// 		const args = ['add']
+// 		return args.concat(packages)
+// 	}
+// }
