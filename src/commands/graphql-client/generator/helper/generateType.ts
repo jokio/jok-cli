892076c061add1d963395acf885f3dbbd082db61
Kyle Bjordahl <3489222+kylebjordahl@users.noreply.github.com>
2020-12-08 16:15:47 -0800

    fix(generator): support nullable lists
    adds support for args of nullable lists with non-nullable elements

--- src/commands/graphql-client/generator/helper/generateType.ts
+++ src/commands/graphql-client/generator/helper/generateType.ts
@@ -6,133 +6,133 @@ import {
 	IntrospectionScalarType,
 	IntrospectionType,
 	IntrospectionUnionType,
-} from 'graphql'
-import getTypescriptField from './getTypescriptField'
+} from "graphql";
+import getTypescriptField from "./getTypescriptField";
 
 export default function (type: IntrospectionType) {
-
-	if (type.name.startsWith('__')) {
-		return
+	if (type.name.startsWith("__")) {
+		return;
 	}
 
 	switch (type.kind) {
-		case 'OBJECT':
-		case 'INTERFACE':
-			return objectType(type)
+		case "OBJECT":
+		case "INTERFACE":
+			return objectType(type);
 
-		case 'INPUT_OBJECT':
-			return inputObjectType(type)
+		case "INPUT_OBJECT":
+			return inputObjectType(type);
 
-		case 'ENUM':
-			return enumType(type)
+		case "ENUM":
+			return enumType(type);
 
-		case 'SCALAR':
-			return scalarType(type)
+		case "SCALAR":
+			return scalarType(type);
 
-		case 'UNION':
-			return unionType(type)
+		case "UNION":
+			return unionType(type);
 
 		default:
-			console.log('MISSED GENERATION FOR', type)
-			return
+			console.log("MISSED GENERATION FOR", type);
+			return;
 	}
 }
 
-function objectType(type: IntrospectionObjectType | IntrospectionInterfaceType) {
+function objectType(
+	type: IntrospectionObjectType | IntrospectionInterfaceType
+) {
 	const fields = type.fields
-		.map(x => getTypescriptField(x.name, x.type, { isNull: false, isList: false }))
-		.map(x => `\t${x}`)
-		.join('\n')
+		.map((x) => getTypescriptField(x.name, x.type))
+		.map((x) => `\t${x}`)
+		.join("\n");
 
-	const typeName = type.name
+	const typeName = type.name;
 
 	return `
 export interface ${typeName} {
 ${fields}
-}`
+}`;
 }
 
 function inputObjectType(type: IntrospectionInputObjectType) {
 	const fields = type.inputFields
-		.map(x => getTypescriptField(x.name, x.type, { isNull: false, isList: false }))
-		.map(x => `\t${x}`)
-		.join('\n')
+		.map((x) => getTypescriptField(x.name, x.type))
+		.map((x) => `\t${x}`)
+		.join("\n");
 
-	const typeName = type.name
+	const typeName = type.name;
 
 	return `
 export interface ${typeName} {
 ${fields}
-}`
+}`;
 }
 
 function enumType(type: IntrospectionEnumType) {
-	const typeName = type.name
+	const typeName = type.name;
 	const fields = type.enumValues
-		.map(x => `\t${x.name} = '${x.name}',`)
-		.join('\n')
+		.map((x) => `\t${x.name} = '${x.name}',`)
+		.join("\n");
 
 	return `
 export enum ${typeName} {
 ${fields}
-}`
+}`;
 }
 
 function scalarType(type: IntrospectionScalarType) {
-	const typeName = type.name
-	let tsType = ''
+	const typeName = type.name;
+	let tsType = "";
 
 	switch (typeName) {
-		case 'ID':
-			tsType = 'string'
-			break
+		case "ID":
+			tsType = "string";
+			break;
 
-		case 'String':
-			tsType = 'string'
-			break
+		case "String":
+			tsType = "string";
+			break;
 
-		case 'Int':
-			tsType = 'number'
-			break
+		case "Int":
+			tsType = "number";
+			break;
 
-		case 'Float':
-			tsType = 'number'
-			break
+		case "Float":
+			tsType = "number";
+			break;
 
-		case 'Boolean':
-			tsType = 'boolean'
-			break
+		case "Boolean":
+			tsType = "boolean";
+			break;
 
-		case 'DateTime':
-		case 'Time':
-			tsType = 'Date'
-			break
+		case "DateTime":
+		case "Time":
+			tsType = "Date";
+			break;
 
 		// Date already exists in JS
-		case 'Date':
-			return ''
+		case "Date":
+			return "";
 
-		case 'JSON':
-			tsType = 'any'
-			break
+		case "JSON":
+			tsType = "any";
+			break;
 
 		default:
-			tsType = 'any'
-			break
+			tsType = "any";
+			break;
 	}
 
 	// export ID for later use in the application
-	if (typeName === 'ID') {
-		return `export type ${typeName} = ${tsType}`
+	if (typeName === "ID") {
+		return `export type ${typeName} = ${tsType}`;
 	}
 
-	return `type ${typeName} = ${tsType}`
+	return `type ${typeName} = ${tsType}`;
 }
 
 function unionType(type: IntrospectionUnionType) {
-	const typeName = type.name
-	const valueTypes = type.possibleTypes
-		.map(x => x.name).join(' | ')
+	const typeName = type.name;
+	const valueTypes = type.possibleTypes.map((x) => x.name).join(" | ");
 
-	return `export type ${typeName} = ${valueTypes}`
+	return `export type ${typeName} = ${valueTypes}`;
 }
--- src/commands/graphql-client/generator/helper/getTypescriptField.ts
+++ src/commands/graphql-client/generator/helper/getTypescriptField.ts
@@ -1,36 +1,72 @@
-import { IntrospectionTypeRef } from 'graphql'
+import { IntrospectionTypeRef } from "graphql";
 
-export default function getTypescriptField(fieldName: string, type: IntrospectionTypeRef, {
-	isNull,
-	isList,
-}: Props) {
+export default function getTypescriptField(
+	fieldName: string,
+	type: IntrospectionTypeRef,
+	{ isNullable = true, isList = false, isListItemNullable = true }: Props = {
+		isNullable: true,
+		isList: false,
+		isListItemNullable: true,
+	}
+) {
 	switch (type.kind) {
-		case 'INPUT_OBJECT':
-		case 'ENUM':
-		case 'OBJECT':
-		case 'INTERFACE':
-		case 'SCALAR':
-		case 'UNION':
-			return buildField(fieldName, type.name, isNull, isList)
+		case "INPUT_OBJECT":
+		case "ENUM":
+		case "OBJECT":
+		case "INTERFACE":
+		case "SCALAR":
+		case "UNION":
+			return buildField(
+				fieldName,
+				type.name,
+				isNullable,
+				isList,
+				isListItemNullable
+			);
 
-		case 'NON_NULL':
-			return getTypescriptField(fieldName, type.ofType, { isList, isNull: true })
+		case "NON_NULL":
+			// if NON_NULL appears before list, then the field/arg is non nullable, otherwise the INNER elements are non-nullable
+			return getTypescriptField(fieldName, type.ofType, {
+				isList,
+				isNullable: isList ? isNullable : false,
+				isListItemNullable: isList ? false : isListItemNullable,
+			});
 
-		case 'LIST':
-			return getTypescriptField(fieldName, type.ofType, { isList: true, isNull })
+		case "LIST":
+			return getTypescriptField(fieldName, type.ofType, {
+				isList: true,
+				isNullable: isNullable,
+				isListItemNullable: isListItemNullable,
+			});
 
 		default:
-			throw new Error(`UNKNOWN_TYPE_KIND`)
+			throw new Error(`UNKNOWN_TYPE_KIND`);
 	}
 }
 
-const buildField = (fieldName, typeName, isNull, isList) => {
-	const nullableSymbol = isNull ? '' : '?'
-	const arraySymbol = isList ? '[]' : ''
+const buildField = (
+	fieldName,
+	typeName,
+	isNullable,
+	isList,
+	isListItemNullable
+) => {
+	const nullableSymbol = isNullable ? "?" : "";
+	if (isList) {
+		// if list items can be nullable, allow for array to have undefined fields
+		// NOTE: maybe this should be null and not undefined?
+		const nullableItems = isListItemNullable
+			? `(${typeName} | undefined)`
+			: typeName;
 
-	return `${fieldName}${nullableSymbol}: ${typeName}${arraySymbol}`
-}
+		return `${fieldName}${nullableSymbol}: ${nullableItems}[]`;
+	} else {
+		return `${fieldName}${nullableSymbol}: ${typeName}`;
+	}
+};
 interface Props {
-	isNull?: boolean
-	isList?: boolean
+	/** graphql types are assumed nullable until explicitly marked non-null */
+	isNullable?: boolean;
+	isList?: boolean;
+	isListItemNullable?: boolean;
 }
